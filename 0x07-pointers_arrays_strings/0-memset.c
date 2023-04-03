20421589ae2dd215c1e70da9a61e18b08c6830cc
NjengaC <victorcyrus01@gmail.com>
2023-04-03 12:19:43 +0300

    correcting 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**_memset -fill a block of memory with a specific value @s: sttarting address of memory to be filled
+ * @s is the starting address of mem to be filled.
  * @b is the desired value.
  * @n is the number of bytes to be changed.
  *
- *returns a pointer to the memory area @*s.
+ * Return a pointer to te memory area @s
  *
  */
 char *_memset(char*s, char b, unsigned int n)
