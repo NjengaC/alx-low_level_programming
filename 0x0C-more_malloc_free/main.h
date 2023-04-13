de13d5d9ed8c9fdb48ff38c217ab6bf3cffd416e
NjengaC <victorcyrus01@gmail.com>
2023-04-13 14:39:08 +0300

    created a function that allocates memory for an array

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
