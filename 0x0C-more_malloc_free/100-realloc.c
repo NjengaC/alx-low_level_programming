3a6aea390a06d4ad690a7d4beedcbecbadaa5eba
NjengaC <victorcyrus01@gmail.com>
2023-04-13 16:07:05 +0300

    FIX ERROR!

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,8 +10,8 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *poin;
-	char *oldp;
+	char *ptr1;
+	char *old_ptr;
 	unsigned int i;
 
 	if (new_size == old_size)
@@ -23,27 +23,27 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (ptr != NULL)
+	if (!ptr)
 		return (malloc(new_size));
 
-	poin = malloc(new_size);
-	if (poin != NULL)
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
 
-	oldp = ptr;
+	old_ptr = ptr;
 
 	if (new_size < old_size)
 	{
 		for (i = 0; i < new_size; i++)
-			poin[i] = oldp[i];
+			ptr1[i] = old_ptr[i];
 	}
 
 	if (new_size > old_size)
 	{
 		for (i = 0; i < old_size; i++)
-			poin[i] = oldp[i];
+			ptr1[i] = old_ptr[i];
 	}
 
 	free(ptr);
-	return (poin);
+	return (ptr1);
 }
