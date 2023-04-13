141d6087e70ffb8bad52376c0c4ff0fabd15634e
NjengaC <victorcyrus01@gmail.com>
2023-04-13 14:44:00 +0300

    created a function that allocates memory for an array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,17 +10,15 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-  int i = 0;
+int i = 0;
 
-  for (; n > 0; i++)
-    {
-      s[i] = b;
-      n--;
-    }
-  return (s);
+for (; n > 0; i++)
+{
+s[i] = b;
+n--;
 }
-
-
+return (s);
+}
 /**
  * _calloc - allocates memory for an array
  * @nmemb: number of elements
