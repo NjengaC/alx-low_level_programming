db65114b98c471a71c775a21007dc2b88e82b8b4
NjengaC <victorcyrus01@gmail.com>
2023-04-17 18:26:16 +0300

    defined a new type dog_t

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,6 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - new struct name representing struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
