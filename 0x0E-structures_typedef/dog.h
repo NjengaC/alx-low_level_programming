e4356b11885c6685080356793dd7a1d7ce2fcf93
NjengaC <victorcyrus01@gmail.com>
2023-04-17 17:47:26 +0300

    defined a new structure struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/***
+ * struct dog - Details of a dog
+ * @name: name of the dog member
+ * @age: dogs age member
+ * @owner: owner name member
+ *
+ * Description: long desc
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
