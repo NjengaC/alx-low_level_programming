56681464cb33ba17608a770d35c5351fa8b8e4f2
NjengaC <victorcyrus01@gmail.com>
2023-04-27 21:13:16 +0300

    Function inserts a new node and the end of list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,7 +15,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tmp = *head;
 	unsigned int len = 0;
 
-	while (str[l])
+	while (str[len])
 		len++;
 
 	new = malloc(sizeof(list_t));
