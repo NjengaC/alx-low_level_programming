d7fa10906aaa2f6be15fcbde1598f2ea89f6caa5
NjengaC <victorcyrus01@gmail.com>
2023-04-27 20:40:46 +0300

    fuunction that inserts at the beginning of the list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,26 +3,26 @@
 #include "lists.h"
 
 /**
- * add_node - adds new node at the beginning of a list
+ * add_node - adds a new node at the beginning of a list
  * @head: pointer to pointer to list
- * @str: new node
+ * @str: new string
  *
- * Return: address of the new element, or NULL on fail
+ * Return: address of the new element, or NULL if it failed
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	unsigned int length = 0;
+	unsigned int len = 0;
 
-	while (str[length])
-		length++;
+	while (str[len])
+		len++;
 
 	new = malloc(sizeof(list_t));
 	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->length = length;
+	new->len = len;
 	new->next = (*head);
 	(*head) = new;
 
