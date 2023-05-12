cc48a0806cdbce6aa5dce65a613ac0330e9e512f
NjengaC <victorcyrus01@gmail.com>
2023-05-13 00:30:42 +0300

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * coin_count - prints the minimum number of coins to make change
+ * @cents: The change to be gotten free
+ *
+ * Return: The number of changes needed
+ */
 int coin_count(int cents)
 {
 	if (cents <= 0)
@@ -28,8 +34,15 @@ int coin_count(int cents)
 		return (1 + coin_count(cents - 1));
 	}
 }
-
-int main (int argc, char **argv)
+/**
+ * main - prints the minimum number of coins to make change
+ * for an amount of money.
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 Success else 1 if no input
+ */
+int main(int argc, char **argv)
 {
 	int cent;
 
