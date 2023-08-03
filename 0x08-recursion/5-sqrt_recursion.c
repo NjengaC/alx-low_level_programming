6160d4d5dbc9758a85f6b506a65ac78b603d610a
NjengaC <victorcyrus01@gmail.com>
2023-08-03 12:06:08 +0300

    Varied the square root function for better understanding

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,9 +7,11 @@
  */
 int _sqrt_recursion(int n)
 {
+	if (n == 0 || n == 1)
+		return (n);
 	if (n < 0)
 		return (-1);
-	return (real_sqrt_recursion(n, 0));
+	return (real_sqrt_recursion(n, (n - 1)));
 }
 
 
@@ -24,9 +26,9 @@ int _sqrt_recursion(int n)
 int real_sqrt_recursion(int n, int x)
 {
 
-	if ((x * x) > n)
+	if ((x * x) < n)
 		return (-1);
 	if ((x * x) == n)
 		return (x);
-	return (real_sqrt_recursion(n, (x + 1)));
+	return (real_sqrt_recursion(n, (x - 1)));
 }
