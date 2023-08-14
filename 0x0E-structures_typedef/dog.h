77be84da8bdb944e28fcfa3ccf737a01ba662b70
NjengaC <victorcyrus01@gmail.com>
2023-08-14 18:12:09 +0300

    Desctription for the structures in header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef _DOG_H_
 #define _DOG_H_
-
+/**
+ * struct dog - structure dog
+ * @name: name of the dog
+ * @age: dogs age
+ * @owner: owners name
+ *
+ * Description: normal desccription
+ */
 struct dog
 {
 	char *name;
@@ -8,6 +15,8 @@ struct dog
 	char *owner;
 };
 typedef struct dog dog_t;
+
+/*dog_t - a new dog typedefed*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
