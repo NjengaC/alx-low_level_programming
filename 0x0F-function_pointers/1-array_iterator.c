4348353b6bdfe5b89706a1994c5d734b7aaa65c3
NjengaC <victorcyrus01@gmail.com>
2023-08-16 20:06:17 +0300

    compilation error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  * rray_iterator - executes a function passed to it as an argument
  * @array: array to be executed
@@ -13,6 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 	for (int i = 0; i < size; i++)
+	{
 		action(array[i]);
-
+	}
 }
