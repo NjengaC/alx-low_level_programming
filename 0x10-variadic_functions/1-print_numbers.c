db1b5655661e70eb47c24e07ebb69f488e782cbf
NjengaC <victorcyrus01@gmail.com>
2023-08-17 21:27:47 +0300

    Output error correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,11 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
+	if (separator == NULL)
+		separator = "";
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(args, int));
-		if ((n != i + 1) && separator != NULL)
+		if (n != i + 1)
 			printf("%s ", separator);
 	}
 	printf("\n");
