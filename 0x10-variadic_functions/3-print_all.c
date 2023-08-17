2f6da5b9bda12984a167caee367e1d09a9381eef
NjengaC <victorcyrus01@gmail.com>
2023-08-17 23:18:14 +0300

    print anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,52 +1,52 @@
 #include "variadic_functions.h"
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+
 /**
- * print_all - prints anything
- * @format: list of arguments passed to the function
+ * print_all - prints anything passed as argument
+ * @format: list
  *
  * Return: nothing
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
 	int i = 0;
-	char *s;
+	char *str, *separator = "";
 
-	va_start(args, format);
+	va_list tags;
+
+	va_start(tags, format);
+
 	if (format)
 	{
 		while (format[i])
 		{
-
 			switch (format[i])
 			{
 				case 'c':
-					printf("%c", va_arg(args, int));
+					printf("%s%c", separator, va_arg(tags, int));
 					break;
 				case 'i':
-					printf("%d", va_arg(args, int));
+					printf("%s%d", separator, va_arg(tags, int));
 					break;
 				case 'f':
-					printf("%f", va_arg(args, double));
+					printf("%s%f", separator, va_arg(tags, double));
 					break;
 				case 's':
-					s = va_arg(args, char *);
-					if (s == NULL)
-						s = "(nil)";
-					printf("%s", s);
+					str = va_arg(tags, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", separator, str);
 					break;
-
 				default:
-				i++;
-				continue;
+					i++;
+					continue;
 			}
-			if (format[i + 1] != '\0')
-				printf(", ");
+			separator = ", ";
 			i++;
 		}
 	}
+
 	printf("\n");
-
-
+	va_end(tags);
 }
