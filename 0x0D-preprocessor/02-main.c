1f821f3881a5a287c00e7b92e2ce49ebc29f90fd
NjengaC <victorcyrus01@gmail.com>
2023-08-29 08:40:29 +0300

    Preprocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Preintd the name of the file the program was compiled from
+ * 
+ * Return: ) Success
+ */
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
