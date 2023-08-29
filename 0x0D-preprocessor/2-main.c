c9df3e30ac822f56b8a3ecd3dd1d426bcf90e54f
NjengaC <victorcyrus01@gmail.com>
2023-08-29 08:45:08 +0300

    betty error correction

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Preintd the name of the file the program was compiled from
- * 
- * Return: ) Success
+ * main - Print the name of the file the program was compiled from
+ *
+ * Return: Success
  */
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
