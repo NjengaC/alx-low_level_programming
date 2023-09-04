de1c28eff9339873c1100592fb07578f9091a35c
NjengaC <victorcyrus01@gmail.com>
2023-09-04 22:41:31 +0300

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,8 +2,8 @@
 
 /**
  * create_file - creates a file
- * @filename: A pointer to the name of the file to create
- * @text_content: A pointer to a string to write to the file
+ * @filename: A pointer to name of the file create
+ * @text_content: A pointer to a string to write file
  *
  * Return: (1) or If the function fails (-1)
  */
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		for (len = 0; text_content[len];)
 			len++;
-	}                                                                                  
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file
+ * append_text_to_file - Appends text at end of file
  * @filename: A pointer to the name of the file
- * @text_content: The string to add to the end of the file
+ * @text_content: The string to add the end of file
  *
- * Return: If the function fails or filename is NULL (-1)
+ * Return: onfail or filename is NULL (-1)
  *
  */
 int append_text_to_file(const char *filename, char *text_content)
@@ -20,13 +20,10 @@ int append_text_to_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	                                                                                   o = open(filename, O_WRONLY | O_APPEND);
-											   w = write(o, text_content, len);
-
-											   if (o == -1 || w == -1)
-												   return (-1);
-
-											   close(o);
-
-											   return (1);
+	o = open(filename, O_WRONLY | O_APPEND);
+	w = write(o, text_content, len);
+	if (o == -1 || w == -1)
+		return (-1);
+	close(o);
+	return (1);
 }
