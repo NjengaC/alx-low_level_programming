9e029491de51a735581fde6d6ba53cdb2f78c10f
NjengaC <victorcyrus01@gmail.com>
2023-10-02 16:36:04 +0300

    eads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,66 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "main.h"
-/**
- * * _putchar - writes the character c to stdout
- * * @c: The character to print
- * *
- * * Return: On success 1.
- * * On error, -1 is returned, and errno is set appropriately.
- * */
-int _putchar(char c)
-{
+#include <stdlib.h>
 
-	return (write(1, &c, 1));
-}
 /**
- * read_textfile - reads from a fileand prints to stdout
- * @filename: text file`s name
- * @letters: number of letters to print
- *
- * Return: actual number of letters
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-
-	FILE *fd;
 	char *buf;
-	ssize_t i = 0, j = 0;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	fd = fopen(filename, "r");
-	if (fd == NULL)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters + 1);
-	if (buf == NULL)
-	{
-		fclose(fd);
-		return (0);
-	}
-
-	j = fread(buf, sizeof(char), letters, fd);
-	if (j <= 0)
-	{
-		free(buf);
-		fclose(fd);
-		return (0);
-	}
-
-	buf[j] = '\0';
-
-	while (buf[i])
-	{
-		_putchar(buf[i]);
-		i++;
-	}
-	if (i != j)
-		return (0);
 	free(buf);
-	fclose(fd);
-	return (i);
+	close(fd);
+	return (w);
 }
