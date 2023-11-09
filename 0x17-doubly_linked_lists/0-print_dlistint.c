cd3ceb8330625c66f3bd3ad25519c9ab0054bf88
NjengaC <victorcyrus01@gmail.com>
2023-11-09 16:49:15 +0300

    betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,12 @@
 #include <string.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * print_dlistint - prints all elements of a DLL
+ * @h: pointer to the head
+ * Return: number of lelements
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
