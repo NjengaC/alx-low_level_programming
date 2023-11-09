8ffb9a2f4d2c4505818ca1e07e51934b376828f8
NjengaC <victorcyrus01@gmail.com>
2023-11-09 18:04:04 +0300

    free DLL function

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint_t - frees a DLL
+ * @head: head
+ * Return: nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head);
+		head = current;
+	}
+}
