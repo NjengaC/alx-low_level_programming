c5820d50c8726da301487a33abbebb78291798c8
NjengaC <victorcyrus01@gmail.com>
2023-11-09 23:50:01 +0300

    cke

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,21 +26,18 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-
 	if (i < idx - 1)
 	{
 		return (NULL);
 	}
-
+	if (i == idx - 1)
+	{
+		return (add_dnodeint_end(h, n));
+	}
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 	{
 		return (NULL);
-	}
-
-	if (current->next == NULL)
-	{
-		return (add_dnodeint_end(h, n));
 	}
 	new->n = n;
 	new->next = (current != NULL) ? current->next : NULL;
