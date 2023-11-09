87d1f2981d835e9f4c6befb6c633e5534898fef7
NjengaC <victorcyrus01@gmail.com>
2023-11-09 23:24:44 +0300

    add node at idx

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * sum_dlistint - returns sum of all the data(n) of a DLL
  * @head: head
- * return: sum of all data
+ * Return: sum of all data
  */
 int sum_dlistint(dlistint_t *head)
 {
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - inserts a node at a given position
+ * @h: head pointer
+ * @idx: index of the list where new node comes in
+ * @n: data(n) member
+ * Return: address of the new node or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new, *current;
+
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+	current = *h;
+
+	while (current != NULL && i < idx - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (i < idx - 1)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = (current != NULL) ? current->next : NULL;
+	new->prev = current;
+
+	if (current != NULL)
+		current->next = new;
+
+	return (new);
+}
