968df3fd54db50cfc520fcf6213b847550a93a47
NjengaC <victorcyrus01@gmail.com>
2023-11-09 23:59:03 +0300

    ddd

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,43 +8,44 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i = 0;
-	dlistint_t *new, *current;
+	dlistint_t *curr, *new = NULL;
+	unsigned int i = 1;
 
-	if (h == NULL)
-	{
-		return (NULL);
-	}
+	curr = *h;
 	if (idx == 0)
 	{
-		return (add_dnodeint(h, n));
+		new = add_dnodeint(h, n);
 	}
-	current = *h;
+	else
+	{
+		if (curr)
+		{
+			while (curr->prev != NULL)
+				curr = curr->prev;
+			while (curr != NULL && i < idx)
+			{
+				curr = curr->next;
+				i++;
+			}
 
-	while (current != NULL && i < idx - 1)
-	{
-		current = current->next;
-		i++;
-	}
-	if (i < idx - 1)
-	{
-		return (NULL);
-	}
-	if (i == idx - 1)
-	{
-		return (add_dnodeint_end(h, n));
-	}
-	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->n = n;
-	new->next = (current != NULL) ? current->next : NULL;
-	new->prev = current;
-	if (current != NULL)
-	{
-		current->next = new;
+			if (i == idx)
+			{
+				if (curr->next == NULL)
+					new = add_dnodeint_end(h, n);
+				else
+				{
+					new = malloc(sizeof(dlistint_t));
+					if (new)
+					{
+						new->n = n;
+						new->next = curr->next;
+						new->prev = curr;
+						curr->next->prev = new;
+						curr->next = new;
+					}
+				}
+			}
+		}
 	}
 	return (new);
 }
