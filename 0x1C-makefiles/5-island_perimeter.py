4340229195dffc278069fc0074dce7ef2eeb22ca
NjengaC <victorcyrus01@gmail.com>
2024-01-25 19:25:53 +0300

    var

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,21 @@ def island_perimeter(grid):
     """
     returns the perimeter of the island described in grid:
     """
-    sides = 0
     count = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == 1:
-                count = count + 1
-                if j > 0 and grid[i][j-1] == 1:
-                    sides = sides + 1
-                if i > 0 and grid[i-1][j] == 1:
-                    sides = sides + 1
-
-    return count * 4 - sides * 2
+                if j < (len(grid[0])-1) and grid[i][j+1] == 0:
+                    count = count + 1
+                if j > 0 and grid[i][j-1] == 0:
+                    count = count + 1
+                if i > 0 and grid[i-1][j] == 0:
+                    count = count + 1
+                if i < (len(grid)-1) and grid[i+1][j] == 0:
+                    count = count + 1
+                if i == 0 or i == len(grid) - 1:
+                    count = count + 1
+                if j == 0 or j == len(grid[0]) - 1:
+                    count = count + 1
+    return count
