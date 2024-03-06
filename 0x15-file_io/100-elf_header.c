a4f767cbb4f93b10fc1296f561f69a363121690b
NjengaC <victorcyrus01@gmail.com>
2024-03-07 02:39:09 +0300

    betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -22,10 +22,12 @@ void print_error(const char *msg)
  * print_header - print ELF header information
  * @header: pointer to ELF header structure
  */
+void print_header(Elf64_Ehdr *header);
 void print_header(Elf64_Ehdr *header)
 {
 	char magic[BUFFER_SIZE] = {0};
 	int i;
+
 	if (memcmp(header->e_ident, ELFMAG, SELFMAG) != 0)
 		print_error("Error: Not an ELF file");
 
@@ -34,20 +36,51 @@ void print_header(Elf64_Ehdr *header)
 	printf("  Magic:   ");
 	for (i = 0; i < SELFMAG; i++)
 		printf("%02x%c", magic[i], i < SELFMAG - 1 ? ' ' : '\n');
-	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %s\n", header->e_ident[EI_OSABI] == ELFOSABI_NONE ? "UNIX - System V" : "UNIX - Other");
-	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Class:                             %s\n",
+	       header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+	       header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement,
+	       little endian" : "2's complement, big endian");
+	printf("  Version:                           %d (current)\n",
+	       header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %s\n",
+	       header->e_ident[EI_OSABI] == ELFOSABI_NONE ?
+	       "UNIX - System V" : "UNIX - Other");
+	printf("  ABI Version:                       %d\n",
+	       header->e_ident[EI_ABIVERSION]);
 	printf("  Type:                              ");
 	switch (header->e_type)
 	{
-		case ET_NONE: printf("NONE (Unknown type)\n"); break;
-		case ET_REL: printf("REL (Relocatable file)\n"); break;
-		case ET_EXEC: printf("EXEC (Executable file)\n"); break;
-		case ET_DYN: printf("DYN (Shared object file)\n"); break;
-		case ET_CORE: printf("CORE (Core file)\n"); break;
-		default: printf("<unknown: %x>\n", header->e_type); break;
+		case ET_NONE:
+			{
+			printf("NONE (Unknown type)\n");
+			break;
+			}
+		case ET_REL:
+			{
+			printf("REL (Relocatable file)\n");
+			break;
+			}
+		case ET_EXEC:
+			{
+			printf("EXEC (Executable file)\n");
+			break;
+			}
+		case ET_DYN:
+			{
+			printf("DYN (Shared object file)\n");
+			break;
+			}
+		case ET_CORE:
+			{
+			printf("CORE (Core file)\n");
+			break;
+			}
+		default:
+			{
+			printf("<unknown: %x>\n", header->e_type);
+			break;
+			}
 	}
 	printf("  Entry point address:               %lx\n", header->e_entry);
 }
